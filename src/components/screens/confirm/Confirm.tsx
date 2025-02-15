f3c723a6633b9c39de87ec64914b64288aa68c2d
Dmitriy <aegis.ko20@mail.ru>
2025-02-15 12:55:18 +0300

    sdd

--- src/components/screens/confirm/Confirm.tsx
+++ src/components/screens/confirm/Confirm.tsx
@@ -1,25 +1,36 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { FC, useEffect } from 'react';
+import { FC, Suspense, useEffect } from 'react';
 
 import Header from '@/components/header/Header';
 import Layout from '@/components/layout/Layout';
 
 import styles from './Confirm.module.scss';
 
-const Confirm: FC = () => {
-	// const confirm = async () => authService.confirm()
+const SearchParamsComponent = () => {
 	const par = useSearchParams();
 	useEffect(() => {
 		console.log(par);
-	}, []);
+	}, [par]);
+	return null;
+};
 
+// export async function generateMetadata({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }): Promise<Metadata> {
+// 	const params = searchParams;
+// 	console.log('Server-side params:', params);
+// 	return {};
+// }
+
+const Confirm: FC = () => {
 	return (
 		<Layout>
 			<Header />
 			<h1 className={styles.title}>Подтверждение пользователя</h1>
 			<p>dssd</p>
+			<Suspense fallback={<div>Загрузка...</div>}>
+				<SearchParamsComponent />
+			</Suspense>
 		</Layout>
 	);
 };
