52866fec35d71c015e98d1fd074113faf34a5cec
Дмитрий <i@frontend-dk.ru>
2025-03-28 16:30:24 +0300

    fix build errors

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,12 +3,9 @@ import { notFound } from 'next/navigation';
 import Export from '@/components/screens/export/Export';
 import Import from '@/components/screens/import/Import';
 
-export default async function DynamicPage({
-	params,
-}: {
-	params: { slug: string };
-}) {
-	const { slug } = await params;
+// export default function DynamicPage({ params }: { params: { slug: string } }) {
+export default function DynamicPage({ params }: any) {
+	const { slug } = params;
 
 	if (slug === 'export') {
 		return <Export />;
